4c94fc2dd71c41dd85f329d2078086b0eaa2d96b
Kovalchuk <olia.kovalchuk270202@gmail.com>
2023-06-06 16:19:56 +0200

    add firebase cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dictionary/app/app.dart';
+import 'package:dictionary/firebase_options.dart';
 import 'package:dictionary/utils/logs_filter.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -12,7 +13,9 @@ final logger = Logger(
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(child: App()),
   );
